ef5b6069ec70e4ceba26aaacf04f1c728f864efc
LeandroCantero <lcantero18@gmail.com>
2022-10-31 02:52:31 -0300

    end about-me

--- src/components/body/about-me/AboutMe.js
+++ src/components/body/about-me/AboutMe.js
@@ -1,28 +1,39 @@
 import React from 'react'
 import styles from './AboutMe.module.css'
+import { ImGithub, ImLinkedin, ImInstagram, ImDownload3 } from 'react-icons/im'
 
 function AboutMe() {
-  return (
-    <div>
-        <section>
-            <div className={styles.container}>
-                <div className={`${styles.containerChild} ${styles.leftContainer}`}>
-                    <h2>Hola mundo, soy Leandro</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit mollitia accusantium labore vitae repudiandae cum atque! Dolor harum neque numquam inventore. Porro dolorum impedit architecto molestiae voluptates deleniti facilis molestias!</p>
-                    <div className='social-links'>
-                        <a href="git">a</a>
-                        <a href="in">a</a>
-                        <a href="ig">a</a>
+    return (
+        <div>
+            <section>
+                <div className={styles.container}>
+                    <div className={`${styles.containerChild} ${styles.leftContainer}`}>
+                        <h2>Hola mundo, soy Leandro</h2>
+                        <p>
+                            Soy desarrollador full-stack, un apasionado del mundo IT.
+                            Decidí estudiar la carrera de Lic. en Informática a los 18 años, en paralelo a esto aprendo a través de cursos y plataformas online.
+                            Me divierte mucho usar el pensamiento lógico y crear soluciones.
+
+                        </p>
+                        <div className={styles.socialLinks}>
+                            <a href="https://github.com/LeandroCantero" target="_blank" rel="noopener noreferrer"><ImGithub /></a>
+                            <a href="https://www.linkedin.com/in/leandro-cantero/" target="_blank" rel="noopener noreferrer"><ImLinkedin /></a>
+                            <a href="https://www.instagram.com/leandrobjs/" target="_blank" rel="noopener noreferrer"><ImInstagram /></a>
+                        </div>
+                        <div className={styles.downloadBtn}>
+                            <a className={styles.downloadCv} href="CV.pdf" download>
+                                <span className={styles.downloadIcon}><ImDownload3 /></span>
+                                <span className={styles.downloadText}>Descargar CV</span>
+                            </a>
+                        </div>
                     </div>
-                    <div className='resume'><a href="https://www.instagram.com/">Download CV</a></div>
+                    <div className={`${styles.containerChild} ${styles.rightContainer}`}>
+                        <img id="programming-gif" src="https://i.pinimg.com/originals/e4/26/70/e426702edf874b181aced1e2fa5c6cde.gif" alt="" />
+                    </div>
                 </div>
-                <div className={`${styles.containerChild} ${styles.rightContainer}`}>
-                    <img id="programming-gif" src="https://i.pinimg.com/originals/e4/26/70/e426702edf874b181aced1e2fa5c6cde.gif" alt="" />
-                </div>
-            </div>
-        </section>
-    </div>
-  )
+            </section>
+        </div>
+    )
 }
 
 export default AboutMe
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styles from './Header.module.css'
+import { ImGithub, ImLinkedin, ImInstagram } from 'react-icons/im'
 
-function Header() {    
+function Header() {
 
     return (
         <div className='header'>
@@ -11,9 +12,9 @@ function Header() {
                         <h2>Cantero Leandro</h2>
                         <div className='socials'>
                             <ul>
-                                <li className='nav-item'><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">Ig</a></li>
-                                <li className='nav-item'><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-                                <li className='nav-item'><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">Github</a></li>
+                                <li className={styles.nav_item}><a href="https://github.com/LeandroCantero" target="_blank" rel="noopener noreferrer"><ImGithub /></a></li>
+                                <li className={styles.nav_item}><a href="https://www.linkedin.com/in/leandro-cantero/" target="_blank" rel="noopener noreferrer"><ImLinkedin /></a></li>
+                                <li className={styles.nav_item}><a href="https://www.instagram.com/leandrobjs/" target="_blank" rel="noopener noreferrer"><ImInstagram /></a></li>
                             </ul>
                         </div>
                     </div>
