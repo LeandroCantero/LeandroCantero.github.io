80b8f3363e887afea58d6125aabf6692ec52ecad
LeandroCantero <lcantero18@gmail.com>
2022-11-01 18:04:19 -0300

    skills end

--- src/components/body/skills/Skills.js
+++ src/components/body/skills/Skills.js
@@ -2,19 +2,21 @@ import React from 'react'
 import Fade from 'react-reveal/Fade';
 import styles from './Skills.module.css';
 import SkillsLottie from '../../animations/SkillsLottie';
+import ReactTooltip from 'react-tooltip'
+import emoji from 'react-easy-emoji'
 
 function Skills() {
     return (
         <div className={styles.skills_section}>
             <div className={styles.text_top}>
                 <h1>Habilidades</h1>
-                <p>Tecnologías aprendidas por el momento.</p>
+                <p>Tecnologías aprendidas por el momento y que puedo hacer con ellas.</p>
             </div>
 
             <div className={styles.container}>
                 <Fade left>
                     <div className={`${styles.left_content} ${styles.child}`}>
-                        <SkillsLottie/>
+                        <SkillsLottie />
                     </div>
 
                 </Fade>
@@ -22,13 +24,30 @@ function Skills() {
                 <Fade right>
                     <div className={`${styles.right_content} ${styles.child}`}>
                         <h3>Fullstack Development</h3>
-                        <div className='languages'>
-                            <p>icons...</p>
+                        <div className={styles.technologies}>
+                            <iconify-icon icon="vscode-icons:file-type-html" data-tip="HTML"></iconify-icon>
+                            <iconify-icon icon="vscode-icons:file-type-css" data-tip="CSS"></iconify-icon>
+                            <iconify-icon icon="logos:javascript" data-tip="Javascript"></iconify-icon>
+                            <iconify-icon icon="logos:react" data-tip="React"></iconify-icon>
+                            <iconify-icon icon="logos:nodejs-icon" data-tip="Node"></iconify-icon>
+                            <iconify-icon icon="logos:vitejs" data-tip="Vite Js"></iconify-icon>
+                            <iconify-icon icon="logos:nextjs-icon" data-tip="Next Js"></iconify-icon>
+                            <iconify-icon icon="logos:dotnet" data-tip=".NET"></iconify-icon>
+                            <iconify-icon icon="logos:java" width="40" height="40" data-tip="Java"></iconify-icon>
+                            <iconify-icon icon="logos:spring-icon" data-tip="Spring"></iconify-icon>
+                            <iconify-icon icon="vscode-icons:file-type-mongo" data-tip="MongoDB"></iconify-icon>
+                            <iconify-icon icon="logos:npm-icon" data-tip="NPM"></iconify-icon>
+                            <iconify-icon icon="logos:git-icon" data-tip="Git"></iconify-icon>
+                            <ReactTooltip 
+                                place="bottom"
+                                effect="solid"
+                                padding="8px"
+                            />
                         </div>
-                        <div className='extra-skills'>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum quae praesentium ab, odit omnis nulla, maiores rem facere, tenetur nobis sequi? Rem sint dolorem dolor, rerum exercitationem aliquid ad aliquam.</p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora modi suscipit illum fuga animi ullam reiciendis laudantium voluptatibus quae at! Mollitia repudiandae aperiam voluptas impedit pariatur blanditiis unde est nihil?</p>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae dolorum dolore, sequi, provident nostrum nisi praesentium cumque molestiae voluptas ducimus est similique possimus, eos ut incidunt quas odio architecto laborum.</p>
+                        <div className={styles.extra_skills}>
+                            <p>{ emoji('🛠️') } Construir sitios web responsive en React.</p>
+                            <p>{ emoji('🛠️') } Crear servicios REST con Spring</p>
+                            <p>{ emoji('🛠️') } Construir RESTful APIs con el MERN stack.</p>
                         </div>
                     </div>
                 </Fade>
